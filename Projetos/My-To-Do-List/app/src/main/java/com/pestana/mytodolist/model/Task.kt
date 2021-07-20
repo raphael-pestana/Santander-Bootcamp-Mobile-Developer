a174e6231bd5d3083ef22644b68e5e48732eeb64
raphael-pestana <65823943+raphael-pestana@users.noreply.github.com>
2021-07-20 17:34:17 -0300

    Created Empty_State for Activity_Main

--- Projetos/My-To-Do-List/app/src/main/java/com/pestana/mytodolist/model/Task.kt
+++ Projetos/My-To-Do-List/app/src/main/java/com/pestana/mytodolist/model/Task.kt
@@ -8,5 +8,18 @@ data class Task(
         val id : Int = 0
 
 ) {
-       // REFAZER ESSA PARTE
+        override fun equals(other: Any?): Boolean {
+                if(this === other) return true
+                if (javaClass != other?.javaClass) return false
+
+                other as Task
+
+                if (id != other.id) return false
+
+                return true
+        }
+
+        override fun hashCode(): Int {
+                return id
+        }
 }
